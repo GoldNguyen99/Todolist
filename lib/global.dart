7d20a78dbcb805b416a79850bcdc0621bae6186c
GoldNguyen99 <goldfpt4040@gmail.com>
2022-05-09 09:04:44 +0700

    future builder

--- lib/global.dart
+++ lib/global.dart
@@ -1,4 +1,4 @@
-const SERVER_NAME = "192.168.1.1"; //your sever's ip address
+const SERVER_NAME = "192.168.1.34"; //your sever's ip address
 const URL_TODOS = "http://$SERVER_NAME:3000/todos";
 const URL_TASKS = 'http://$SERVER_NAME:3000/tasks';
 const URL_TASK_BY_TODOID = 'http://$SERVER_NAME:3000/tasks/todoid/';
--- lib/taskscreen.dart
+++ lib/taskscreen.dart
@@ -3,6 +3,7 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/foundation.dart';
+import 'models/task.dart';
 
 class TaskScreen extends StatefulWidget {
   final int todoid;
@@ -29,7 +30,50 @@ class _TaskScreenState extends State<TaskScreen> {
           )
         ],
       ),
-      body: Text("LIST TASK HERE!!"),
+      body: FutureBuilder(
+          future: fetchTask(http.Client(), widget.todoid),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) print(snapshot.error);
+            return snapshot.hasData
+                ? TaskList(tasks: snapshot.data as List<Task>)
+                : Center(
+                    child: CircularProgressIndicator(),
+                  );
+          }),
+    );
+  }
+}
+
+class TaskList extends StatelessWidget {
+  final List<Task> tasks;
+  TaskList({Key? key, required this.tasks}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          child: Container(
+            padding: EdgeInsets.all(10.0),
+            color: index % 2 == 0 ? Colors.deepOrangeAccent : Colors.amber,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  this.tasks[index].name,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+                ),
+                new Text(
+                  'Finished: ${tasks[index].finished == true ? "Yes" : "No"}',
+                  style: TextStyle(fontSize: 16.0),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+      itemCount: this.tasks.length,
     );
   }
 }
